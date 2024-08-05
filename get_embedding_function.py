285b717f1d261cb646fa475cfcbf2e48647b2624
paragsarin <42388306+paragsarin@users.noreply.github.com>
2024-08-05 16:01:00 -0400

    Update get_embedding_function.py

--- get_embedding_function.py
+++ get_embedding_function.py
@@ -2,9 +2,7 @@
 from langchain.embeddings import HuggingFaceEmbeddings
 
 def get_embedding_function():
-    '''embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )'''
+  
 
     embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
